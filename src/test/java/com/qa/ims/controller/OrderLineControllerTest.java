76bbd1a8044ed48c3f304baaf700bff190b2a995
Korbinian <kmr.ring@outlook.com>
2020-04-17 12:05:59 +0100

    OrderLineController Test

--- src/test/java/com/qa/ims/controller/OrderLineControllerTest.java
+++ src/test/java/com/qa/ims/controller/OrderLineControllerTest.java
@@ -2,6 +2,8 @@ package com.qa.ims.controller;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,46 +32,55 @@ public class OrderLineControllerTest {
 	}
 
 	@Test
-	public void changeItemsTest() {
+	public void changeItemsTestAdd() {
 		String action = "add";
-		String orderId = "6";
-		String itemId = "4";
-		String quantity = "3";
+		String orderId = "1";
+		String itemId = "1";
+		String quantity = "1";
 
 		Mockito.doReturn(action, orderId, itemId, quantity).when(orderLineController).getInput();
 		Long orderIdConverted = Long.valueOf(orderId);
 		Long itemIdConverted = Long.valueOf(itemId);
 		Long quantityConverted = Long.valueOf(quantity);
 		OrderLine orderLine = new OrderLine(orderIdConverted, itemIdConverted, quantityConverted);
-		OrderLine savedOrderLine = new OrderLine(6L, 4L, 3L);
+		OrderLine savedOrderLine = new OrderLine(1L, 1L, 1L);
 		Mockito.when(orderLineServices.changeItems(orderLine)).thenReturn(savedOrderLine);
+		assertEquals(savedOrderLine, orderLineController.changeItems());
+	}
 
+	@Test
+	public void changeItemsTestDelete() {
+		String action = "remove";
+		String orderId = "1";
+		String itemId = "1";
+
+		Mockito.doReturn(action, orderId, itemId).when(orderLineController).getInput();
+		Long orderIdConverted = Long.valueOf(orderId);
+		Long itemIdConverted = Long.valueOf(itemId);
+		OrderLine orderLine = new OrderLine(orderIdConverted, itemIdConverted, null);
+		OrderLine savedOrderLine = new OrderLine(1L, 1L, null);
+		Mockito.when(orderLineServices.changeItems(orderLine)).thenReturn(savedOrderLine);
 		assertEquals(savedOrderLine, orderLineController.changeItems());
 	}
 
 //	@Test
 //	public void readItemsInOrderTest() {
-//		String firstName = "Chris";
-//		String surname = "Perrins";
-//		Mockito.doReturn(firstName, surname).when(orderLineController).getInput();
-//		OrderLine orderLine = new OrderLine(firstName, surname);
-//		OrderLine savedOrderLine = new OrderLine(1L, "Chris", "Perrins");
-//		Mockito.when(orderLineServices.create(orderLine)).thenReturn(savedOrderLine);
-//		assertEquals(savedOrderLine, orderLineController.create());
+//		Mockito.doReturn("1").when(orderLineController).getInput();
+//		OrderLineController orderLineController = new OrderLineController(orderLineServices);
+//		List<OrderLine> orderLines = new ArrayList<>();
+//		orderLines.add(new OrderLine(1L, 2L, 1L));
+//		orderLines.add(new OrderLine(1L, 1L, 3L));
+//		Mockito.when(orderLineServices.readOrder(1L)).thenReturn(orderLines);
+//		assertEquals(orderLines, orderLineController.readItemsInOrder());
 //	}
-//
-//	/**
-//	 * 
-//	 */
-//	@Test
-//	public void calculateOrderPriceTest() {
-//		String id = "1";
-//		String firstName = "Rhys";
-//		String surname = "Thompson";
-//		Mockito.doReturn(id, firstName, surname).when(orderLineController).getInput();
-//		OrderLine orderLine = new OrderLine(1L, firstName, surname);
-//		Mockito.when(orderLineServices.update(orderLine)).thenReturn(orderLine);
-//		assertEquals(orderLine, orderLineController.update());
-//	}
+
+	@Test
+	public void calculateOrderPriceTest() {
+		String id = "1";
+		Mockito.doReturn(id).when(orderLineController).getInput();
+
+		Mockito.when(orderLineServices.calculate(Long.valueOf(id))).thenReturn(BigDecimal.valueOf(47.96));
+		assertEquals(BigDecimal.valueOf(47.96), orderLineController.calculateOrderPrice());
+	}
 
 }
